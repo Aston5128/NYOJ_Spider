e73bbbd2191d2df11d17db99c0d3ffba6b5bda34
Aston5128 <2338733304@qq.com>
2017-04-15 11:59:10 +0800

    Update NYOJ_Spider.py
    Add threading module

--- NYOJ_Spider.py
+++ NYOJ_Spider.py
@@ -1,10 +1,12 @@
 # A NYOJ data spider
 # href: http://acm.nyist.net/JudgeOnline/problemset.php
 # Code by Aston5128
+# Only take 0.1s
 
 from Download import dl
 from bs4 import BeautifulSoup
 import time
+import threading
 
 
 class nyoj:
@@ -21,16 +23,19 @@ class nyoj:
         # output to file with localtime
         self.output_file.write(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()) + '\n')
 
+    def getAc(self, url, name):
+        html = dl.GetHtml(url)
+        ac_num = BeautifulSoup(html, 'lxml').find('table', class_='side-table').find_all('td')[3].text
+        ac = name + ' ' + str(ac_num)
+        print(ac)
+        self.output_file.write(ac + '\n')
+
     def get(self):
         """Main def"""
         for name in self.name_list:
             url = self.start_url + name
-            html = dl.GetHtml(url)
-            ac_num = BeautifulSoup(html, 'lxml').find('table', class_='side-table').find_all('td')[3].text
-            ac = name + ' ' + str(ac_num)
-            print(ac)
-            self.output_file.write(ac + '\n')
-        self.output_file.close()
+            th = threading.Thread(target=self.getAc, args=(url, name))
+            th.start()
 
 
 if __name__ == '__main__':
